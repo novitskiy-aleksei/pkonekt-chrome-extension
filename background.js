0f2384c0a0cbf2c5c0793fc840917f4e4c887832
NovitskiyAleksei <a.novitskiy@ekleft.ru>
2013-10-09 10:36:26 +0300

    Improvements
    - add icon on //extension page
    - add notifications

--- background.js
+++ background.js
@@ -14,15 +14,21 @@ function tryToPost(link)
 
     req.onreadystatechange = function()
     {
-        // If the request completed, close the extension popup
-        if (req.readyState == 4)
-            if (req.status == 200) window.close();
+        if (req.readyState == 4){
+            if (req.status == 200) {
+                var m = JSON.parse(req.responseText).message;
+
+                if (typeof m != 'undefined'){
+                    alert('Pkonekt сказал: \n' + JSON.parse(req.responseText).message)
+                }
+            }else{
+                alert('Возникла непредвиденная ошибка или pkonekt опять лежит. Обратитесь к Шутку')
+            }
+        }
     };
 }
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({url: "http://pkonekt.vitalyshutko.com"}, function(tab) {
-        // Tab opened.
-    });
+    chrome.tabs.create({url: "http://pkonekt.vitalyshutko.com"}, function(tab) {});
 });
 chrome.contextMenus.create({title: "Запостить на pkonekt", contexts:["image", "link"], onclick: tryToPost});
